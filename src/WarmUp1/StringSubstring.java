bdc7add62c7030e8268febb6b9d0b85c76c17110
dsocol <dsocol@yahoo.com>
2023-03-29 09:35:09 +0300

    Added:
    Following methods
     conCat;

--- src/WarmUp1/StringSubstring.java
+++ src/WarmUp1/StringSubstring.java
@@ -1,6 +1,21 @@
 package WarmUp1;
 
 public class StringSubstring {
+    /**
+     * Given two strings, append them together (known as "concatenation") and return the result.
+     * However, if the concatenation creates a double-char, then omit one of the chars,
+     * so "abc" and "cat" yields "abcat".
+         * conCat("abc", "cat") → "abcat"
+         * conCat("dog", "cat") → "dogcat"
+         * conCat("abc", "") → "abc"
+     * **/
+    public String conCat(String a, String b) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (a.length() > 0 && b.length() > 0 && a.charAt(a.length()-1) == b.charAt(0)) {
+            return stringBuilder.append(a).append(b.substring(1)).toString();
+        }
+        return stringBuilder.append(a).append(b).toString();
+    }
 
     /**
      * Given a string and an index, return a string length 2 starting at the given index.
